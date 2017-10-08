87b20ea778217a6e7c66fcf26a09bab210f97a5d
Paul <code@logston.me>
2017-10-07 21:48:56 -0400

    Update configure.sh

--- vm/configure.sh
+++ vm/configure.sh
@@ -1,6 +1,9 @@
 # To configure your VM with python3.6, run the following single line of code 
 # at the command line (don't copy the "#"):
 # curl -fsSL https://raw.githubusercontent.com/logston/py-for-or/master/vm/configure.sh | bash
+# Then run these commands to set a password:
+# jupyter notebook password
+# sudo systemctl restart jupyter
 sudo apt-get update
 sudo apt-get -y upgrade
 sudo apt-get install -y \
