99fafbd4e36e357f80b78cba397ca48ee803bdfe
Paul <code@logston.me>
2017-10-07 21:56:25 -0400

    Update configure.sh

--- vm/configure.sh
+++ vm/configure.sh
@@ -51,23 +51,11 @@ c.NotebookApp.allow_root = True
 c.NotebookApp.ip = '0.0.0.0'
 ## The port the notebook server will listen on.
 c.NotebookApp.port = 80
-## Dict of Python modules to load as notebook server extensions.Entry values can
-#  be used to enable and disable the loading ofthe extensions. The extensions
-#  will be loaded in alphabetical order.
-#c.NotebookApp.nbserver_extensions = {}
 ## Whether to open in a browser after starting. The specific browser used is
 #  platform dependent and determined by the python standard library `webbrowser`
 #  module, unless it is overridden using the --browser (NotebookApp.browser)
 #  configuration option.
 c.NotebookApp.open_browser = False
-## Hashed password to use for web authentication.
-#  
-#  To generate, type in a python/IPython shell:
-#  
-#    from notebook.auth import passwd; passwd()
-#  
-#  The string should be of the form type:salt:hashed-password.
-#c.NotebookApp.password = ''
 EOF
 
 pip install jupyter_contrib_nbextensions
