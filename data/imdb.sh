a36f37466d56abb41a5c2081aafca2cf190d3d5e
Paul Logston <paul.logston@gmail.com>
2020-01-21 06:22:51 +0000

    imdb.sh edited online with Bitbucket

--- data/imdb.sh
+++ data/imdb.sh
@@ -1,4 +1,4 @@
-wget https://github.com/logston/columbia-vm-management/raw/master/data/imdb.sql.gz
+wget https://bitbucket.org/LogstonEducation/tfa-vm-management/raw/master/data/imdb.sql.gz
 gzip -d imdb.sql.gz
 psql -d postgres -c "create database imdb"
 psql -f imdb.sql imdb
