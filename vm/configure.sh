39cbefcb2d390cf4de128b1b4fae8698a64dcb2f
Paul Logston <paul.logston@gmail.com>
2020-01-22 03:53:24 +0000

    configure.sh edited online with Bitbucket

--- vm/configure.sh
+++ vm/configure.sh
@@ -1,6 +1,19 @@
 # To configure your VM with python3, run the following single line of code 
 # at the command line from the VM (don't copy the "#" or the "$"):
 #     $ curl -fsSL https://bitbucket.org/LogstonEducation/tfa-vm-management/raw/master/vm/configure.sh | bash
+LOCK_FILE=/tmp/config-lock
+
+if [[ -f "$LOCK_FILE" ]]; then
+        echo "Configuration is already running. Configuration started at ..."
+        cat $LOCK_FILE
+        echo "If the configuration is taking more than an hour, "
+        echo "Please run the following command and then restart the configuration scirpt."
+        exit 1;
+else
+        echo "Starting Configuration"
+        echo $(date) > $LOCK_FILE
+fi
+
 sudo apt-get update
 sudo apt-get install -y \
         build-essential \
@@ -28,18 +41,18 @@ git config --global core.editor "vim"
 # Install Python
 PY_VERSION=$(python3 -V)
 if [ "$PY_VERSION" != "Python 3.8.1" ]; then
-echo "Installing Python"
-cd /tmp
-wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz -O python.tgz
-tar -xvzf python.tgz
-cd Python-3.8.1
-./configure --prefix=/usr/local
-make -j 4
-sudo make install
-sudo ln -s /usr/local/bin/python3.8 /usr/local/bin/python
-sudo ln -s /usr/local/bin/pip3 /usr/local/bin/pip
-sudo chown -R $(whoami):$(whoami) /usr/local/
-cd ~
+        echo "Installing Python"
+        cd /tmp
+        wget https://www.python.org/ftp/python/3.8.1/Python-3.8.1.tgz -O python.tgz
+        tar -xvzf python.tgz
+        cd Python-3.8.1
+        ./configure --prefix=/usr/local
+        make -j 4
+        sudo make install
+        sudo ln -s /usr/local/bin/python3.8 /usr/local/bin/python
+        sudo ln -s /usr/local/bin/pip3 /usr/local/bin/pip
+        sudo chown -R $(whoami):$(whoami) /usr/local/
+        cd ~
 fi
 
 echo "Installing Postgres"
@@ -58,5 +71,9 @@ psql -d postgres -c "CREATE DATABASE $USER;"
 
 curl https://bitbucket.org/LogstonEducation/tfa-vm-management/raw/master/data/imdb.sh | bash
 curl https://bitbucket.org/LogstonEducation/tfa-vm-management/raw/master/data/bank.sh | bash
+
+if [[ -f "$LOCK_FILE" ]]; then
+        rm $LOCK_FILE
+fi
 
 echo "Configuration Complete"
